fa82ff0d6e57cb41db02e25304fe7abd56c1d906
YAzakharov28 <yaroslavskiy28@gmail.com>
2026-05-17 20:27:52 +0300

    Update OrderStatusChoices

--- backend/choices.py
+++ backend/choices.py
@@ -2,13 +2,13 @@ from django.db.models import TextChoices
 
 
 class UserRoleChoices(TextChoices):
-    BUYER = "buyer", 'Покупатель'
-    SHOP = 'shop', 'Магазин'
+    BUYER = "buyer", "Покупатель"
+    SHOP = "shop", "Магазин"
 
 
 class OrderStatusChoices(TextChoices):
     CREATED = "created", "Заказ оформлен"
-    PAID = "paid", "Оплата прошла"
+    CONFIRMED = "confirmed", "Подтвержден"
     PROCESSING = "processing", "Заказ собирается"
     SHIPPED = "shipped", "Передан в доставку"
     DELIVERED = "delivered", "Получен"
