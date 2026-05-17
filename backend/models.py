04db032bc453032ec24207e110c53c9daeb124df
YAzakharov28 <yaroslavskiy28@gmail.com>
2026-05-17 20:01:57 +0300

    Add custom user model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from backend.choices import UserRoleChoices
+
+
+class User(AbstractUser):
+    role = models.CharField(
+        max_length=20,
+        choices=UserRoleChoices.choices,
+        default=UserRoleChoices.BUYER,
+    )
+
+
