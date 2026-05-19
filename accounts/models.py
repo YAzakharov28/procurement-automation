9ec9ad6e213cdf788078327a9243fca34b4d8414
YAzakharov28 <yaroslavskiy28@gmail.com>
2026-05-19 20:53:22 +0300

    Update user model

--- accounts/models.py
+++ accounts/models.py
@@ -40,7 +40,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         choices=UserRoleChoices.choices,
         default=UserRoleChoices.BUYER,
     )
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
