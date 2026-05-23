44341c140f1c5c6495bed0807f36d0579e67d0bd
YAzakharov28 <yaroslavskiy28@gmail.com>
2026-05-23 12:17:17 +0300

    Add CustomUserAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from accounts.models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    ordering = ("email",)
+    list_display = ("email", "first_name", "last_name", "role", "is_staff", "is_active")
+    search_fields = ("email", "first_name", "last_name")
+    list_filter = ("role", "is_staff", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "role")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "first_name", "last_name", "role", "password1", "password2"),
+            },
+        ),
+    )
+
+    readonly_fields = ("last_login", "date_joined")
