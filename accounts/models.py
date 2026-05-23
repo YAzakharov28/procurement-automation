80e072c39b2185bbc2791af18c9227610041a7df
YAzakharov28 <yaroslavskiy28@gmail.com>
2026-05-23 10:17:12 +0300

    Update user model

--- accounts/models.py
+++ accounts/models.py
@@ -18,6 +18,7 @@ class UserManager(BaseUserManager):
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+        return user
 
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
@@ -42,7 +43,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     )
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
 
     objects = UserManager()
 
