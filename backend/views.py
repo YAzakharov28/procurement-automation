959f31348fddbdad711da08a3765969ee6597e1d
YAzakharov28 <yaroslavskiy28@gmail.com>
2026-05-24 11:31:13 +0300

    Add shop view set

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from backend.models import Shop
+from backend.permissions import IsShopOwnerOrAdminOrReadOnly
+from backend.serializers import ShopSerializer
+from backend.tasks import update_shop_positions_task
+
+
+class ShopViewSet(ModelViewSet):
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["name"]
+    permission_classes = [IsShopOwnerOrAdminOrReadOnly]
+
+    def perform_create(self, serializer: ShopSerializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            return Shop.objects.none()
+        return Shop.objects.all()
+
+    @action(methods=["patch"], detail=True)
+    def positions(self, request: Request, pk: int | None = None):
+        shop = self.get_object()
+        self.check_object_permissions(request, shop)
+        update_shop_positions_task.delay(shop.id)
+        return Response({"message": "Импорт запущен"}, status=status.HTTP_202_ACCEPTED)
