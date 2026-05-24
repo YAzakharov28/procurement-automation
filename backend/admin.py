8bdc80596fce2b710f0a9f71181533904c9eaa5f
YAzakharov28 <yaroslavskiy28@gmail.com>
2026-05-24 11:32:15 +0300

    Add ShopAdmin

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from backend.models import Shop
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "user", "url")
+    list_filter = ("name",)
+    search_fields = ("name", "user__email")
