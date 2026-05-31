6007e7cb042301d409d8c2b0b844fa85bb2f5b09
YAzakharov28 <yaroslavskiy28@gmail.com>
2026-05-31 11:35:02 +0300

    Change user registry path

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from accounts import views
 
 urlpatterns = [
     path("registry/", views.RegisterView.as_view()),
-    path("confirm-email/", views.ConfirmEmailView.as_view()),
+    path("confirm/", views.ConfirmEmailView.as_view()),
     path("login/", views.LoginView.as_view()),
 ]
